5c7827672d1face62d2b1977223224c599e82516
cutz-j <cutz309@gmail.com>
2018-10-20 22:40:45 +0900

    Update KNN

--- lec5-1[p230차원의저주].py
+++ lec5-1[p230차원의저주].py
@@ -1,3 +1,125 @@
 import numpy as np
 import pandas as pd
-import random.math
+import random
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+random.seed(777)
+
+def random_point_gen(dimension):
+    '''
+    정규분포 0 ~ 1 사이의 난수 발생 (차원의 수 만큼 반복해서 리스트에 comprehension)
+    '''
+    return [random.random() for _ in range(dimension)]
+
+def distance(v, w):
+    '''
+    유클리디언 거리 제곱 -> sum of sq, sqrt 계산
+    두 점간의 거리는 좌표끼리의 차이를 각각 제곱해 더하고, sqrt
+    '''
+    vec_sub = [v_i - w_i for v_i, w_i in zip(v, w)] # 두 벡터의 차이
+    sum_of_sq = sum(v_i * v_i for v_i in vec_sub) # 거리 제곱
+    return math.sqrt(sum_of_sq) # 거리 제곱근 리턴
+
+def random_dist_comparison(dimension, number_pairs):
+    '''
+    random 정규 분포 2개의 vector 거리 계산
+    '''
+    return [distance(random_point_gen(dimension), random_point_gen(dimension)) for _ in range(number_pairs)]
+
+def mean(x):
+    return sum(x) / len(x)
+
+dimensions = range(1, 201, 5) # dim 5차원부터, 200차원까지
+
+## 최소 & 평균거리 계산 ##
+avg_dist = []
+min_dist = []
+
+dummyarray = np.empty((20,4))
+dist_vals = pd.DataFrame(dummyarray)
+dist_vals.columns = ["Dimension", "Min_Distance", "Avg_Distance", "Min/Avg_Distance"]
+
+i = 0
+for dims in dimensions:
+    distances = random_dist_comparison(dims, 100) # 100쌍의 차원마다 계산 1 ~ 196 5단위
+    avg_dist.append(mean(distances)) # 평균
+    min_dist.append(min(distances)) # 최소
+    dist_vals.loc[i, "Dimension"] = dims # df에 기록
+    dist_vals.loc[i, "Min_Distance"] = min(distances)
+    dist_vals.loc[i, "Avg_Distance"] = mean(distances)
+    dist_vals.loc[i, "Min/Avg_Distance"] = min(distances) / mean(distances)
+    i += 1
+
+
+# graph
+#plt.figure()
+#plt.xlabel("dim")
+#plt.ylabel("dist")
+#plt.plot(dist_vals["Dimension"], dist_vals["Avg_Distance"], "r-")
+#plt.legend(loc='best')
+#plt.show()
+
+## 2차원 ,3차원 ##
+two = np.random.rand(60, 2)
+two2 = np.random.rand(60, 2)
+two_df = pd.DataFrame(two)
+two_df.columns = ["x", "y"]
+
+#plt.figure()
+#plt.scatter(two_df['x'], two_df['y'])
+#plt.show()
+
+three = np.random.rand(60, 3)
+three2 = np.random.rand(60, 3)
+three_df = pd.DataFrame(three)
+three_df.columns = ['x', 'y', 'z']
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.scatter(three_df['x'], three_df['y'], three_df['z'])
+#plt.show()
+#print(np.sqrt(np.sum(np.square(two - two2)))) # 4.7
+#print(np.sqrt(np.sum(np.square(three - three2)))) # 5.7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lec5[p236KNN].py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
+b_cancer = pd.read_csv("d:/data/ML/breast-cancer-wisconsin.data.txt", sep=',', header=None)
+colList = ["ID number", "Clump Thickness", "Uniformity of Cell Size", "Uniformity of Cell Shape",
+           "Marginal Adhesion", "Single Epithelial Cell Size", "Bare Nuclei", "Bland Chromatin",
+           "Normal Nucleoli", "Mitoses", "Class"]
+for col in range(len(colList)):
+    colList[col] = colList[col].replace(" ", "_")
+b_cancer.columns = colList
+
+# 결측치 대체 # --> ?를 nan으로, nan을 최빈값으로
+b_cancer['Bare_Nuclei'] = b_cancer['Bare_Nuclei'].replace('?', np.NAN)
+b_cancer['Bare_Nuclei'] = b_cancer['Bare_Nuclei'].fillna(b_cancer['Bare_Nuclei'].value_counts().index[0])
+b_cancer['Cancer_Ind'] = 0
+b_cancer.loc[b_cancer['Class'] == 4, 'Cancer_Ind'] = 1
+
+# 무의미 변수 제거 #
+x_vars = b_cancer.drop(['ID_number', 'Class', 'Cancer_Ind'], axis=1)
+y_var = b_cancer['Cancer_Ind']
+x_vars_stdscle = StandardScaler().fit_transform(x_vars.values) # 표준화( x - mean / std ) --> scaling
+x_vars_stdscle_df = pd.DataFrame(x_vars_stdscle, index=x_vars.index, columns=x_vars.columns)
+x_train, x_test, y_train, y_test = train_test_split(x_vars_stdscle_df, y_var, train_size=0.7, random_state=77)
+
+## KNN ##
+knn_fit = KNeighborsClassifier(n_neighbors=3, p=2, metric='minkowski')
+knn_fit.fit(x_train, y_train)
+
+#print("\n\nConfusion Matrix\n\n", pd.crosstab(y_train, knn_fit.predict(x_train), rownames=["Actuall"], colnames=["Predicted"]))
+#print("\naccruacy: ", round(accuracy_score(y_train, knn_fit.predict(x_train)), 3)) # 학습데이터 98%
+#print("\nreport: \n", classification_report(y_train, knn_fit.predict(x_train)))
+
+print("\n\nConfusion Matrix\n\n", pd.crosstab(y_test, knn_fit.predict(x_test), rownames=["Actuall"], colnames=["Predicted"]))
+print("\naccruacy: ", round(accuracy_score(y_test, knn_fit.predict(x_test)), 3)) # 학습데이터 97.6%
+print("\nreport: \n", classification_report(y_test, knn_fit.predict(x_test)))
+
+## KNN K tuning ##
+dummyarray = np.empty((5,3))
+k_valchart = pd.DataFrame(dummyarray)
+k_valchart.columns = ["K_value", "Train_acc", "Test_acc"]
+
+k_vals = [1,2,3,4,5]
+
+for i in range(len(k_vals)):
+    knn_fit = KNeighborsClassifier(n_neighbors=k_vals[i], p=2, metric='minkowski')
+    knn_fit.fit(x_train, y_train)
+    
+    print("\n", k_vals[i])
+    print("\n\nConfusion Matrix\n\n", pd.crosstab(y_train, knn_fit.predict(x_train), rownames=["Actuall"], colnames=["Predicted"]))
+    print("\naccruacy: ", round(accuracy_score(y_train, knn_fit.predict(x_train)), 3)) # 학습데이터 97.6%
+    print("\nreport: \n", classification_report(y_train, knn_fit.predict(x_train)))
+    
+    print("\n\nConfusion Matrix\n\n", pd.crosstab(y_test, knn_fit.predict(x_test), rownames=["Actuall"], colnames=["Predicted"]))
+    print("\naccruacy: ", round(accuracy_score(y_test, knn_fit.predict(x_test)), 3)) # 학습데이터 97.6%
+    print("\nreport: \n", classification_report(y_test, knn_fit.predict(x_test)))
+    k_valchart.loc[i, 'K_value'] = k_vals[i]
+    k_valchart.loc[i, 'Train_acc'] = round(accuracy_score(y_train, knn_fit.predict(x_train)), 3)
+    k_valchart.loc[i, 'Test_acc'] = round(accuracy_score(y_test, knn_fit.predict(x_test)), 3)
+    
+# graph
+plt.figure()
+plt.plot(k_valchart["K_value"], k_valchart["Train_acc"], "r-")
+plt.plot(k_valchart["K_value"], k_valchart["Test_acc"], "b-")
+
+plt.axis([0.9, 5, 0.92, 1.005])
+plt.xticks([1,2,3,4,5])
+
+for a, b in zip(k_valchart["K_value"], k_valchart["Train_acc"]):
+    plt.text(a,b,str(b), fontsize=10)
+for a, b in zip(k_valchart["K_value"], k_valchart["Test_acc"]):
+    plt.text(a,b,str(b), fontsize=10)
+plt.legend(loc='upper right')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
